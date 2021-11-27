ce84b10b4b27f537645186f499743bb82b5b90a1
Yemisrach Serawit <se.yemisrach.serawit@gmail.com>
2021-11-27 17:24:39 +0300

    Filter jobs based on selected filters

--- static-job-listings/js/script.js
+++ static-job-listings/js/script.js
@@ -1,6 +1,7 @@
 let cardContainer = document.querySelector('.card__container');
 let jobFilterContainer = document.querySelector('.job-filter__container');
 let clearFiltersBtn = document.querySelector('.clear-btn');
+let selectedFilters = [];
 let filters;
 
 fetch("../data.json")
@@ -31,28 +32,91 @@ clearFiltersBtn.addEventListener('click', handleClearBtnClick);
 
 function handleClearBtnClick(e) {
     e.target.previousElementSibling.innerHTML = '';
+    selectedFilters = [];
+    filterCards();
 }
 
 function handleFilterClick(e) {
+    let filterText = e.target.innerText;
     // If filter is already selected, do nothing
-    if (isFilterSelected(e.target.innerText)) return;
+    if (isFilterSelected(filterText)) return;
 
     // If not, add filter btn and listener (to remove filter)
     if (jobFilterContainer.innerText === '') {
-        jobFilterContainer.innerHTML = constructFilterBtn(e.target.innerText);
+        jobFilterContainer.innerHTML = constructFilterBtn(filterText);
     } else {
         let lastFilterBtn = jobFilterContainer.lastElementChild;
-        lastFilterBtn.insertAdjacentHTML('afterend', constructFilterBtn(e.target.innerText));
+        lastFilterBtn.insertAdjacentHTML('afterend', constructFilterBtn(filterText));
     }
+    // add filter to list
+    selectedFilters.push(filterText);
 
+    // Handle listener for removing filter
     let removeFilterBtn = jobFilterContainer.lastElementChild.querySelector('.remove-filter-btn');
     removeFilterBtn.addEventListener('click', (e) => {
-        e.target.parentElement.remove();
+        handleRemoveFilterClick(e, filterText);
     });
+
+    filterCards();
 }
 
+function handleRemoveFilterClick(e, filterText) {
+    e.target.parentElement.remove();
+    const filterIndex = selectedFilters.indexOf(filterText);
+    selectedFilters.splice(filterIndex, 1); // remove filter from list
+    filterCards();
+}
+
+// function to customise cards based on selected filters
+function filterCards() {
+    const cards = document.querySelectorAll('.card');
+
+    // if there are no filters, unhide all cards
+    if (selectedFilters.length === 0) {
+        const hiddenCards = document.querySelectorAll('.card.hidden');
+        for (let i = 0; i < hiddenCards.length; i++) {
+            hiddenCards[i].classList.remove('hidden');
+        }
+        return;
+    }
+
+    // if there are filters, unhide matching cards and hide non-matching cards
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        if (!hasSelectedFilters(card)) {
+            card.classList.add('hidden');
+        } else {
+            card.classList.remove('hidden');
+        }
+    }
+}
+
+// function to check if card has all the selected filters
+function hasSelectedFilters(card) {
+    for (let i = 0; i < selectedFilters.length; i++) {
+        const filter = selectedFilters[i];
+        if (!hasTag(filter, card)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// function to check if a card has a specific tag
+function hasTag(tagText, card) {
+    const tags = card.querySelectorAll('.filter');
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i].innerText;
+        if (tag === tagText) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// function to check if a filter is already selected
 function isFilterSelected(filterText) {
-    return jobFilterContainer.innerText.includes(filterText);
+    return selectedFilters.includes(filterText);
 }
 
 function constructFilterBtn(text) {
