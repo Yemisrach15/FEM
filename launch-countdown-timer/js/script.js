9f0585659c6922cadbe84c2802314d644eb5a9c4
Yemisrach Serawit <se.yemisrach.serawit@gmail.com>
2022-01-25 22:36:38 +0300

    Change animation trigger

--- launch-countdown-timer/js/script.js
+++ launch-countdown-timer/js/script.js
@@ -1,40 +1,86 @@
-let seconds = document.querySelector('.seconds');
-let minutes = document.querySelector('.minutes');
-let hours = document.querySelector('.hours');
-let days = document.querySelector('.days');
+let seconds = document.querySelectorAll('.seconds');
+let minutes = document.querySelectorAll('.minutes');
+let hours = document.querySelectorAll('.hours');
+let days = document.querySelectorAll('.days');
 
-function updateSecond(secondCard) {
-    let currentSecond = Number(secondCard.innerHTML) + 1;
+function updateSecond(secondCards) {
+    let currentSecond = Number(secondCards[0].innerHTML) + 1;
     if (currentSecond == 60) {
         currentSecond = 0;
         updateMinute(minutes);
     }
 
-    secondCard.innerHTML = formatDate(currentSecond);
+    secondCards.forEach((card) => {
+        card.innerHTML = formatDate(currentSecond);
+        if (card.classList.contains('top')) {
+            card.classList.add('animate-flip-top');
+        }
+        if (card.classList.contains('bottom')) {
+            card.classList.add('animate-flip-bottom');
+        }
+    })
 }
 
-function updateMinute(minuteCard) {
-    let currentMinute = Number(minuteCard.innerHTML) + 1;
+function updateMinute(minuteCards) {
+    let currentMinute = Number(minuteCards[0].innerHTML) + 1;
     if (currentMinute == 60) {
         currentMinute = 0;
         updateHour(hours);
     }
 
-    minuteCard.innerHTML = formatDate(currentMinute);
+    minuteCards.forEach((card) => {
+        card.innerHTML = formatDate(currentMinute);
+        if (card.classList.contains('top')) {
+            card.classList.remove('animate-flip-top');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-top');
+        }
+        if (card.classList.contains('bottom')) {
+            card.classList.remove('animate-flip-bottom');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-bottom');
+        }
+    })
 }
 
-function updateHour(hourCard) {
-    let currentHour = Number(hourCard.innerHTML) + 1;
+function updateHour(hourCards) {
+    let currentHour = Number(hourCards[0].innerHTML) + 1;
     if (currentHour == 24) {
         currentHour = 0;
         updateDay(days);
     }
-    hourCard.innerHTML = formatDate(currentHour);
+
+    hourCards.forEach((card) => {
+        card.innerHTML = formatDate(currentHour);
+        if (card.classList.contains('top')) {
+            card.classList.remove('animate-flip-top');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-top');
+        }
+        if (card.classList.contains('bottom')) {
+            card.classList.remove('animate-flip-bottom');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-bottom');
+        }
+    })
 }
 
-function updateDay(dayCard) {
-    let currentDay = Number(dayCard.innerHTML) + 1;
-    dayCard.innerHTML = formatDate(currentDay);
+function updateDay(dayCards) {
+    let currentDay = Number(dayCards[0].innerHTML) + 1;
+
+    dayCards.forEach((card) => {
+        card.innerHTML = formatDate(currentDay);
+        if (card.classList.contains('top')) {
+            card.classList.remove('animate-flip-top');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-top');
+        }
+        if (card.classList.contains('bottom')) {
+            card.classList.remove('animate-flip-bottom');
+            void card.offsetWidth;
+            card.classList.add('animate-flip-bottom');
+        }
+    })
 }
 
 function formatDate(date) {
