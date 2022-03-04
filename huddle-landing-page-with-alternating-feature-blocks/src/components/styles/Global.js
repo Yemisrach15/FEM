8a5b0c0202174583e718440cec8bf1b5cae15091
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-03-04 11:41:19 +0300

    Import font in html

--- huddle-landing-page-with-alternating-feature-blocks/src/components/styles/Global.js
+++ huddle-landing-page-with-alternating-feature-blocks/src/components/styles/Global.js
@@ -2,8 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import { color } from "styled-system";
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Poppins:wght@600&display=swap');
-
     * {
         box-sizing: border-box;
         font-family: 'Poppins', sans-serif;
