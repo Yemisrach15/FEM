84043de165e0c1884489862ca5906bf5ccec15c8
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-08-10 20:00:12 +0300

    fix: style adjustments

--- ip-address-tracker/src/components/Main.js
+++ ip-address-tracker/src/components/Main.js
@@ -6,114 +6,116 @@ import SectionType from './SectionType';
 
 class Main extends React.Component {
 
-    state = {
-        clientIpaddr: "",
-        inputIpaddr: "",
-        ipaddr: "",
-        location: "",
-        timezone: "",
-        isp: "",
-        lat: 0,
-        lng: 0,
-        loading: true
-    }
+  state = {
+    clientIpaddr: "",
+    inputIpaddr: "",
+    ipaddr: "",
+    location: "",
+    timezone: "",
+    isp: "",
+    lat: 0,
+    lng: 0,
+    loading: true
+  }
 
-    inputRef = React.createRef();
-    smallRef = React.createRef();
+  inputRef = React.createRef();
+  smallRef = React.createRef();
 
-    getClientIP = async () => {
-        await axios.get(`/apiIpify/?format=json`)
-            .then(res => {
-                this.setState({ clientIpaddr: res.data.ip })
+  getClientIP = async () => {
+    await axios.get(`/apiIpify/?format=json`)
+      .then(res => {
+        this.setState({ clientIpaddr: res.data.ip })
+      });
+  }
+
+  async componentDidMount() {
+    await this.getClientIP();
+    await axios.get(`/apiGeoIpify/country,city?ipAddress=${this.state.clientIpaddr}`)
+        .then(res => {
+            const locationString = `${res.data.location.city}, ${res.data.location.country} ${res.data.location.postalCode}`;
+
+            this.setState({
+                ipaddr: res.data.ip,
+                location: locationString,
+                timezone: res.data.location.timezone,
+                isp: res.data.isp,
+                lat: res.data.location.lat,
+                lng: res.data.location.lng,
+                loading: false
             });
-    }
+        });
+  }
 
-    async componentDidMount() {
-        await this.getClientIP();
-        await axios.get(`/apiGeoIpify/country,city?ipAddress=${this.state.clientIpaddr}`)
-            .then(res => {
-                const locationString = `${res.data.location.city}, ${res.data.location.country} ${res.data.location.postalCode}`;
+  handleInputChange = (e) => {
+    this.setState({ inputIpaddr: e.target.value });
+  }
 
-                this.setState({
-                    ipaddr: res.data.ip,
-                    location: locationString,
-                    timezone: res.data.location.timezone,
-                    isp: res.data.isp,
-                    lat: res.data.location.lat,
-                    lng: res.data.location.lng,
-                    loading: false
-                });
+  handleSubmit = () => {
+    this.setState({ loading: true });
+
+    axios.get(`/apiGeoIpify/country,city?ipAddress=${this.state.inputIpaddr}`)
+        .then(res => {
+            const locationString = `${res.data.location.city}, ${res.data.location.country} ${res.data.location.postalCode}`;
+            this.setState({
+                ipaddr: res.data.ip,
+                location: locationString,
+                timezone: res.data.location.timezone,
+                isp: res.data.isp,
+                lat: res.data.location.lat,
+                lng: res.data.location.lng,
+                loading: false
             });
-    }
-
-    handleInputChange = (e) => {
-        this.setState({ inputIpaddr: e.target.value });
-    }
-
-    handleSubmit = () => {
-        this.setState({ loading: true });
-
-        axios.get(`/apiGeoIpify/country,city?ipAddress=${this.state.inputIpaddr}`)
-            .then(res => {
-                const locationString = `${res.data.location.city}, ${res.data.location.country} ${res.data.location.postalCode}`;
-                this.setState({
-                    ipaddr: res.data.ip,
-                    location: locationString,
-                    timezone: res.data.location.timezone,
-                    isp: res.data.isp,
-                    lat: res.data.location.lat,
-                    lng: res.data.location.lng,
-                    loading: false
-                });
-            })
-            .catch(err => {
-                this.setState({
-                    ipaddr: "-",
-                    location: "-",
-                    timezone: "-",
-                    isp: "-",
-                    loading: false
-                });
-
-                if (err.response.data.code === 422) {
-                    this.inputRef.current.classList.add('input--error');
-                    this.smallRef.current.classList.remove('hidden');
-                }
+        })
+        .catch(err => {
+            this.setState({
+                ipaddr: "-",
+                location: "-",
+                timezone: "-",
+                isp: "-",
+                loading: false
             });
-    }
 
-    preventSubmitOnEnter = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            this.handleSubmit();
-        }
-    }
+            if (err.response.data.code === 422) {
+                this.inputRef.current.classList.add('input--error');
+                this.smallRef.current.classList.remove('hidden');
+            }
+        });
+  }
 
-    render() {
-        return (
-            <div>
-                <main className='addr'>
-                    <h1>IP Address Tracker</h1>
-                    <form>
-                        <div>
-                            <input onChange={this.handleInputChange} name='addr' type='text' placeholder='Search for any IP address or domain' defaultValue={this.state.clientIpaddr} onKeyPress={this.preventSubmitOnEnter} ref={this.inputRef} />
-                            <button aria-label="Get IP Information" onClick={this.handleSubmit} type='button'></button>
-                        </div>
-                        <small className='hidden' ref={this.smallRef}>Please enter a valid IPv4 or IPv6 address</small>
-                    </form>
-                    <div className='addr__info'>
-                        <Section loading={this.state.loading} heading={SectionType.IPADDR} para={this.state.ipaddr} />
-                        <Section loading={this.state.loading} heading={SectionType.LOCATION} para={this.state.location} />
-                        <Section loading={this.state.loading} heading={SectionType.TIMEZONE} para={this.state.timezone} />
-                        <Section loading={this.state.loading} heading={SectionType.ISP} para={this.state.isp} />
-                    </div>
-                </main>
-                <div className='map__container'>
-                    <Map loading={this.state.loading} lat={this.state.lat} lng={this.state.lng} />
-                </div>
+  preventSubmitOnEnter = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      this.handleSubmit();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className='addr'>
+          <div>
+            <h1>IP Address Tracker</h1>
+            <form>
+              <div>
+                <input onChange={this.handleInputChange} name='addr' type='text' placeholder='Search for any IP address or domain' defaultValue={this.state.clientIpaddr} onKeyPress={this.preventSubmitOnEnter} ref={this.inputRef} />
+                <button aria-label="Get IP Information" onClick={this.handleSubmit} type='button'></button>
+              </div>
+              <small className='hidden' ref={this.smallRef}>Please enter a valid IPv4 or IPv6 address</small>
+            </form>
+            <div className='addr__info'>
+              <Section loading={this.state.loading} heading={SectionType.IPADDR} para={this.state.ipaddr} />
+              <Section loading={this.state.loading} heading={SectionType.LOCATION} para={this.state.location} />
+              <Section loading={this.state.loading} heading={SectionType.TIMEZONE} para={this.state.timezone} />
+              <Section loading={this.state.loading} heading={SectionType.ISP} para={this.state.isp} />
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className='map__container'>
+          <Map loading={this.state.loading} lat={this.state.lat} lng={this.state.lng} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default Main;
