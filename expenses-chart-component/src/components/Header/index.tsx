8c0128ac3087e5a42dd189e8484a58cb2f10623a
Yemisrach <se.yemisrach.serawit@gmail.com>
2023-02-25 11:53:04 +0300

    Migrate from cra webpack bundler to vite

--- expenses-chart-component/src/components/Header/index.tsx
+++ expenses-chart-component/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import logo from '../../assets/icons/logo.svg';
 import { Title, StyledHeader } from './style';
 
 const Header = () => {
